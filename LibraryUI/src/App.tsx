4d9453d0c42b773980d3d4d3b7db148fb0934807
SRIKARREDDY-dotorg <srikarreddypochana12@gmail.com>
2025-02-23 13:59:39 +0530

    Implement View Books UI

--- LibraryUI/src/App.tsx
+++ LibraryUI/src/App.tsx
@@ -3,6 +3,7 @@ import {Home} from "./components/Home.tsx";
 import {Login} from "./components/Login.tsx";
 import {Register} from "./components/Register.tsx";
 import {Layout} from "./components/Layout.tsx";
+import {Books} from "./components/Books.tsx";
 
 export default function LibraryApp() {
     return (
@@ -12,6 +13,7 @@ export default function LibraryApp() {
                     <Route path="/" element={<Home />} />
                     <Route path="/login" element={<Login />} />
                     <Route path="/signup" element={<Register />} />
+                    <Route path="/books" element={<Books />} />
                     {/* Add more routes as needed */}
                 </Route>
             </Routes>
